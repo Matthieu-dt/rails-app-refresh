3c21567f82eef449deb1181bec99faf8355435e7
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2021-07-20 19:36:45 +0200

    added simple_form gem and create all crud enpoints for garden model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
-
   root to:'pages#home'
+
+  resources :gardens
+
+  # #Create a garden
+  # get 'gardens/new' to: 'gardens#new'
+  # post 'gardens' to: 'gardens#create'
+
+  # #Get all gardens
+  # get 'gardens' to: 'gardens#index'
+
+  # #Get all garden
+  # get 'gardens/:id' to: 'gardens#show'
+
+  # #Update a garden
+  # get 'gardens/:id/edit' to: 'gardens#edit'
+  # patch 'gardens/:id' to: 'gardens#update'
+
+  # #Delete a garden
+
+  # delete 'gardens/:id' to 'gardens#destroy'
+
+  # Static pages
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
