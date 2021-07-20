b148d00f221b8de636b865536b2a047c450567e7
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2021-07-20 10:42:49 +0200

    added column address to gardens table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_083248) do
+ActiveRecord::Schema.define(version: 2021_07_20_084003) do
 
   create_table "gardens", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
